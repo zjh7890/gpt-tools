bc5972a3717b712b305ad3c391d4cc0bb3526736
zjh7890 <2510280010@qq.com>
2024-08-13 12:55:10 +0800

    feat: test github action

--- src/main/kotlin/com/github/zjh7890/gpttools/actions/PsiDependencyByMethodAction.kt
+++ src/main/kotlin/com/github/zjh7890/gpttools/actions/PsiDependencyByMethodAction.kt
@@ -42,6 +42,7 @@ class PsiDependencyByMethodAction : AnAction() {
 //        val message = element.text
         ClipboardUtils.copyToClipboard(message)
         println(message)
+        println(message)
     }
 
     fun generateRpcCallsLinksFromTree(node: NodeInfo): List<String> {
