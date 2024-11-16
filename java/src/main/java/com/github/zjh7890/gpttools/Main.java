84c60517e1a1f895ca3d590e9fdc3cdf1952f6bb
zjh7890 <2510280010@qq.com>
2024-11-16 23:04:01 +0800

    feat: modulize

---
+++ java/src/main/java/com/github/zjh7890/gpttools/Main.java
@@ -0,0 +1,10 @@
+package com.github.zjh7890.gpttools;
+
+/**
+ * @Date: 2024/11/16 22:47
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
